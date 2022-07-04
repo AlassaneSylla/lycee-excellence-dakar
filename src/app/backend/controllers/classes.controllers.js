b7cce302a37f2ee38d6d823572b3e49b7bcb7251
PapiAlou <papialou90@gmail.com>
2022-07-04 11:05:02 +0100

    create controllers

---
+++ src/app/backend/controllers/classes.controllers.js
@@ -0,0 +1,36 @@
+const Classes = require('../models/classes');
+
+
+exports.getAllClasses = (req, res, next) => {
+    Classes.find()
+    .then(classes => res.status(200).json(classes))
+    .catch(error => res.status(400).json({ error }));
+};
+
+exports.getOneClass = (req, res, next) => {
+    Classes.findOne({ _id: req.params.id })
+    .then(classes => res.status(200).json(classes))
+    .catch(error => res.status(404).json({ error }));
+};
+
+exports.createClass = (req, res, next) => {
+    const classe = new Classes({
+        ...req.body
+    });
+    classe.save()
+    .then(() => res.status(201).json({ message: 'Objet créé !' }))
+    .catch(error => res.status(400).json({ error }));
+};
+
+exports.modifyClass = (req, res, next) => {
+    Classes.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !' }))
+    .catch(error => res.status(400).json({ error }));
+};
+
+exports.deleteClass = (req, res, next) => {
+    Classes.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
+    .catch(error => res.status(400).json({ error }));
+};
+
--- src/app/backend/routes/classes.routes.js
+++ src/app/backend/routes/classes.routes.js
@@ -1,42 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const Classes = require('../models/classes');
+const classesControllers = require('../controllers/classes.controllers');
 
-
-router.get('/', (req, res, next) => {
-    Classes.find()
-    .then(classes => res.status(200).json(classes))
-    .catch(error => res.status(400).json({ error }));
- });
- 
-router.post('/', (req, res, next) => {
-     const classe = new Classes({
-         ...req.body
-     });
-     classe.save()
-     .then(() => res.status(201).json({ message: 'Objet créé !' }))
-     .catch(error => res.status(400).json({ error }));
- });
- 
- router.get('/:id', (req, res, next) => {
-     Classes.findOne({ _id: req.params.id })
-     .then(classes => res.status(200).json(classes))
-     .catch(error => res.status(404).json({ error }));
- })
- 
- router.put("/:id", (req, res, next) => {
-     Classes.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-     .then(() => res.status(200).json({ message: 'Objet modifié !' }))
-     .catch(error => res.status(400).json({ error }));
- });
- 
- router.delete('/:id', (req, res, next) => {
-     Classes.deleteOne({ _id: req.params.id })
-     .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
-     .catch(error => res.status(400).json({ error }));
- });
-
-
+router.get('/', classesControllers.getAllClasses); 
+router.post('/', classesControllers.createClass);
+router.get('/:id', classesControllers.getOneClass);
+router.put("/:id", classesControllers.modifyClass);
+router.delete('/:id', classesControllers.deleteClass);
 
 module.exports = router;
